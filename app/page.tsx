314e3ec93d3e5c0083c8448aa75c740bc7dca4ec
hari-0101 <harivignesh@apiplatform.io>
2024-11-04 19:26:15 +0530

    removed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import DefaultNav from '@components/DefaultNav'
+// import DefaultNav from '@components/DefaultNav'
+//
+// export default function Home() {
+//   return (
+//       <>
+//           <DefaultNav/>
+//       </>
+//   );
+// }
+
+// pages/index.js
+
+"use client"
+// pages/index.js
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-      <>
-          <DefaultNav/>
-      </>
-  );
+    const router = useRouter();
+
+    useEffect(() => {
+        // Redirect to /chat
+        router.push('/chat');
+    }, [router]);
+
+    return null;
 }
+
